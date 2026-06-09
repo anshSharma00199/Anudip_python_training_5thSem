c576f4ddf79dd4a53911bceef62ba80cf6871d63
anshSharma00199 <a2024cseaiml10580@gmail.com>
2026-06-09 15:06:54 +0530

    Area and perimeter of 2-D shapes by using function Module

---
+++ classwork/function_9th_june/main.py
@@ -0,0 +1,21 @@
+import Area
+
+print("Rectangle")
+print("Area:", Area.area_rectangle(10, 5))
+print("Perimeter:", Area.perimeter_rectangle(10, 5))
+
+print("--------------------------------------------")
+print("\nSquare")
+print("Area:", Area.area_square(4))
+print("Perimeter:", Area.perimeter_square(4))
+
+print("--------------------------------------------")
+print("\nCircle")
+print("Area:", Area.area_circle(7))
+print("Perimeter:", Area.perimeter_circle(7))
+
+print("--------------------------------------------")
+print("\nTriangle")
+print("Area:", Area.area_triangle(8, 6))
+print("Perimeter:", Area.perimeter_triangle(3, 4, 5))
+
